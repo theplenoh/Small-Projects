a44b3c096630affce46b4f51689eea9a644feca4
Plenoh <tylrd629@gmail.com>
2026-06-27 16:21:57 +0900

    Complete the rest of the func. getLastDay

--- calendar.py
+++ calendar.py
@@ -5,3 +5,23 @@ def getLastDay(year, month):
         return 29
     elif month == 2:
         return 28
+    elif month == 3:
+        return 31
+    elif month == 4:
+        return 30
+    elif month == 5:
+        return 31
+    elif month == 6:
+        return 30
+    elif month == 7:
+        return 31
+    elif month == 8:
+        return 31
+    elif month == 9:
+        return 30
+    elif month == 10:
+        return 31
+    elif month == 11:
+        return 30
+    elif month == 12:
+        return 31

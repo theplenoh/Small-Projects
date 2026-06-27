fa84b3c480e9f59b339015b963e70217834905cd
Plenoh <tylrd629@gmail.com>
2026-06-27 16:58:25 +0900

    Rewrite the func. getLastDay()

--- calendar.py
+++ calendar.py
@@ -1,30 +1,10 @@
 def getLastDay(year, month):
-    if month == 1:
-        return 31
-    elif (month == 2) and ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)):
-        return 29
-    elif month == 2:
-        return 28
-    elif month == 3:
-        return 31
-    elif month == 4:
-        return 30
-    elif month == 5:
-        return 31
-    elif month == 6:
-        return 30
-    elif month == 7:
-        return 31
-    elif month == 8:
-        return 31
-    elif month == 9:
-        return 30
-    elif month == 10:
-        return 31
-    elif month == 11:
-        return 30
-    elif month == 12:
-        return 31
+    lastDays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        lastDays[2] = 29
+
+    return lastDays[month]
 
 #currYear = 2024
 
